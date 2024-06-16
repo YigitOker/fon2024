0ae41a8c56aa0090a16841200e63fe9a6d9845c7
Volkan Yiğit Oker <168348307+YigitOker@users.noreply.github.com>
2024-06-16 14:40:04 +0300

    Update fonlar.R
    Kod güncellendi YBB eklendi

--- fonlar.R
+++ fonlar.R
@@ -1,14 +1,16 @@
+library(tidyverse)
+library(janitor)
+
 fonlar2 <- read.csv("~/r/fonlar2.csv", header=FALSE)
 fonlar2 <- fonlar2 %>% row_to_names(row_number = 1)
+
+colnames(fonlar2)[colnames(fonlar2) == "6AY"] <- "AYLIK6"
+colnames(fonlar2)[colnames(fonlar2) == "3AY"] <- "AYLIK3"
 
 fonlar2 <- fonlar2 %>% mutate(HAFTALIK = as.numeric(HAFTALIK))
 fonlar2 <- fonlar2 %>% mutate(AYLIK = as.numeric(AYLIK))
 fonlar2 <- fonlar2 %>% mutate(AYLIK3 = as.numeric(AYLIK3))
-fonlar2 <- fonlar2 %>% mutate(AYLIK6 = as.numeric(AYLIK6))
-
-
-colnames(fonlar2)[colnames(fonlar2) == "6AY"] <- "AYLIK6"
-                              
+fonlar2 <- fonlar2 %>% mutate(AYLIK6 = as.numeric(AYLIK6))                              
 
 fonlar2 <- fonlar2 %>% mutate(weeklysd = pnorm(HAFTALIK, mean=mean(HAFTALIK), sd=sd(HAFTALIK)))
 fonlar2 <- fonlar2 %>% mutate(ayliksd = pnorm(AYLIK, mean=mean(AYLIK), sd=sd(AYLIK)))
@@ -17,4 +19,5 @@ fonlar2 <- fonlar2 %>% mutate(aylik6sd = pnorm(AYLIK6, mean=mean(AYLIK6), sd=sd(
 
 fonlar2 <- fonlar2 %>% mutate(sumsd = rowSums(across(c(weeklysd, ayliksd, aylik3sd, aylik6sd))))
 fonlar2 <- fonlar2 %>% mutate(sdsumsd = pnorm(sumsd, mean=mean(sumsd), sd=sd(sumsd)))
+fonlar2 <- fonlar2 %>% mutate(ybbsd = pnorm(YBB, mean=mean(YBB), sd=sd(YBB)))
 
